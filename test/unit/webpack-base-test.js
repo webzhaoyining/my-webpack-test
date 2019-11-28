1912b712c72a267a0dd95a01861701ba185191f5
ynnnnn <a35156256@163.com>
2019-11-28 20:57:49 +0800

    test: webpack-base-test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,7 +4,7 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base');
     // console.log(baseConfig);
     it('entry', ()=> {
-        assert.equal(baseConfig.entry.index,'/Users/zxy9/my-project/build-webpack/test/smoke/template/src/index/index.js');
-        assert.equal(baseConfig.entry.search,'/Users/zxy9/my-project/build-webpack/test/smoke/template/src/search/index.js');
+        assert.equal(baseConfig.entry.index.indexOf('test/smoke/template/src/index/index.js'),true);
+        assert.equal(baseConfig.entry.search.indexOf('test/smoke/template/src/search/index.js'),true);
     })
 })
