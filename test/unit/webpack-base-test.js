a0a86fe10be673d9f3867fa72cc532859688b9c0
zhaoyining <zhaoyining@zhixueyun.com>
2019-11-29 16:02:39 +0800

    fix: webpack-base-test change

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,7 +4,7 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base');
     // console.log(baseConfig);
     it('entry', ()=> {
-        assert.equal(baseConfig.entry.index.indexOf('index/index.js'),true);
-        assert.equal(baseConfig.entry.search.indexOf('index.js'),true);
+        assert(baseConfig.entry.index.indexOf('index/index.js'),true);
+        assert(baseConfig.entry.search.indexOf('index.js'),true);
     })
 })
