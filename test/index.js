0830e5e0e79fc70d6460137f90b4700c016fd6f2
yyyyn <42113700+webzhaoyining@users.noreply.github.com>
2020-06-02 18:11:04 +0800

    Update index.js

--- test/index.js
+++ test/index.js
@@ -4,6 +4,6 @@ const path = require('path');
 process.chdir(path.join(__dirname,'smoke/template'));
 
 describe('builder-webpack test case', ()=> {
-    console.log('test');
+    console.log('testsss');
     require('./unit/webpack-base-test');
 });
